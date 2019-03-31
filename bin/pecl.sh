7d6d304178fd4e63ec2069be4ba50d096091e671
Eric Chow <yateric@gmail.com>
2019-03-31 21:13:11 +0800

    add full path

--- bin/pecl.sh
+++ bin/pecl.sh
@@ -2,4 +2,4 @@ echo "Downloading PECL"
 curl -O https://pear.php.net/go-pear.phar
 
 echo "Installing PECL"
-php -d detect_unicode=0 go-pear.phar
+/app/.heroku/php-min/bin/php -d detect_unicode=0 go-pear.phar
